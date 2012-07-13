c24af25168135cf87f13e30251b5408d496bc5ee
Michael Kelly <mkelly@mozilla.com>
2012-07-13 18:33:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Olympics Facebook Tab Promotion
+
+A Facebook tab celebrating the Olympics with Personas!
+
+See https://wiki.mozilla.org/Websites/FFOlympics for more details.
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+$personas = array(
+    'argentina' => array('name' => 'Argentina', 'persona' => '{&quot;id&quot;:&quot;474681&quot;,&quot;name&quot;:&quot;Flag Argentina&quot;,&quot;category&quot;:null,&quot;description&quot;:&quot;&quot;,&quot;author&quot;:&quot;MozThemes&quot;,&quot;username&quot;:&quot;mozthemes&quot;,&quot;headerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/8\/1\/474681\/Persona_argentina1a.jpg?1341599107&quot;,&quot;footerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/8\/1\/474681\/footer_persona_argentina.jpg?1341599107&quot;,&quot;detailURL&quot;:&quot;http:\/\/www.getpersonas.com\/persona\/474681&quot;,&quot;previewURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/8\/1\/474681\/preview.jpg?1341599107&quot;,&quot;iconURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/8\/1\/474681\/preview_small.jpg?1341599107&quot;,&quot;dataurl&quot;:&quot;data:image\/png;base64,\/9j\/4AAQSkZJRgABAQEASABIAAD\/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD\/2wBDAQMDAwQDBAgEBAgQCwkLEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBD\/wAARCAAQABADAREAAhEBAxEB\/8QAFgABAQEAAAAAAAAAAAAAAAAABAUI\/8QAJBAAAQMEAQMFAAAAAAAAAAAAAQIEEQADBQYhBxIxEyIjQaL\/xAAYAQACAwAAAAAAAAAAAAAAAAAEBgMFB\/\/EACQRAAEEAQIGAwAAAAAAAAAAAAEAAgMEERIxBQYHISLwgZHh\/9oADAMBAAIRAxEAPwDX2sstQvul3c\/k77MeZb9xJEfYANJPTNvEWV3uYzwO3vZWvNhpOcBK7DkDcG3Tm1dQGeyZXtMz8J5\/FasZLw3YPv8AUlCKidnn34R7TX0Ux7VBQ8r5JFG1qkFOMRQNwAgJ7Mtl+uU5U5\/iWjlUqShUHgQYonTlQasFf\/\/Z&quot;,&quot;accentcolor&quot;:null,&quot;textcolor&quot;:null,&quot;updateURL&quot;:&quot;https:\/\/www.getpersonas.com\/en-US\/update_check\/474681&quot;,&quot;version&quot;:&quot;1341599107&quot;}'),
+    'brazil' => array('name' => 'Brazil', 'persona' => '{&quot;id&quot;:&quot;474692&quot;,&quot;name&quot;:&quot;Flag Brazil&quot;,&quot;category&quot;:null,&quot;description&quot;:&quot;&quot;,&quot;author&quot;:&quot;MozThemes&quot;,&quot;username&quot;:&quot;mozthemes&quot;,&quot;headerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/9\/2\/474692\/Persona_brazil1c.jpg?1341604936&quot;,&quot;footerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/9\/2\/474692\/footer_persona_brazil.jpg?1341604936&quot;,&quot;detailURL&quot;:&quot;http:\/\/www.getpersonas.com\/persona\/474692&quot;,&quot;previewURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/9\/2\/474692\/preview.jpg?1341604936&quot;,&quot;iconURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/9\/2\/474692\/preview_small.jpg?1341604936&quot;,&quot;dataurl&quot;:&quot;data:image\/png;base64,\/9j\/4AAQSkZJRgABAQEASABIAAD\/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD\/2wBDAQMDAwQDBAgEBAgQCwkLEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBD\/wAARCAAQABADAREAAhEBAxEB\/8QAFgABAQEAAAAAAAAAAAAAAAAABwUG\/8QAJRAAAQMDAwMFAAAAAAAAAAAAAQIDBAUGEQASIQcTQSIyYXGB\/8QAGAEAAgMAAAAAAAAAAAAAAAAABgcBBAX\/xAAmEQABAwIGAgIDAAAAAAAAAAABAgMRAAQFBhIhMUFhgQhRFCJx\/9oADAMBAAIRAxEAPwC90IitRbZrtehRHaVAlPtFURbpXHbeSk73G3DztKSkEK5Tgeo+F98gH2F4nY2iiHLhKVyQP20KI0BQHchREciTAqrkROm1efSChskbE7AgGSCeoiZ4+zQr1pqXTydUHXLfaLtVKz3JELCY6jnnf4WflP6TqMlWuYLdpIvjpZjZK5K\/EdpHhXoUG5yusu3ClfhCXp3UjZHmeifKfZNae+L7ua8GEU2S5HgUdlO2NSoDXYiNJ8AIT7vtWdEOD5YscIdVdDU7cK3U64dbij96jx6ilzi2b8UxwBDyghkcNoGlAH8HPsmjOVRmlOkpCVc8DBxoirJF4U1\/\/9k=&quot;,&quot;accentcolor&quot;:null,&quot;textcolor&quot;:null,&quot;updateURL&quot;:&quot;https:\/\/www.getpersonas.com\/en-US\/update_check\/474692&quot;,&quot;version&quot;:&quot;1341604936&quot;}'),
+    'italy' => array('name' => 'Italy', 'persona' => '{&quot;id&quot;:&quot;475015&quot;,&quot;name&quot;:&quot;Flag Italy&quot;,&quot;category&quot;:null,&quot;description&quot;:&quot;&quot;,&quot;author&quot;:&quot;MozThemes&quot;,&quot;username&quot;:&quot;mozthemes&quot;,&quot;headerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475015\/Persona_Italy.jpg?1341943165&quot;,&quot;footerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475015\/footer_persona_italy.jpg?1341943165&quot;,&quot;detailURL&quot;:&quot;http:\/\/www.getpersonas.com\/persona\/475015&quot;,&quot;previewURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475015\/preview.jpg?1341943165&quot;,&quot;iconURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475015\/preview_small.jpg?1341943165&quot;,&quot;dataurl&quot;:&quot;data:image\/png;base64,\/9j\/4AAQSkZJRgABAQEASABIAAD\/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD\/2wBDAQMDAwQDBAgEBAgQCwkLEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBD\/wAARCAAQABADAREAAhEBAxEB\/8QAFwAAAwEAAAAAAAAAAAAAAAAAAwUGB\/\/EACQQAAEDAwMEAwAAAAAAAAAAAAECBBEAAwYFEhMHMmFxkZLh\/8QAGAEAAwEBAAAAAAAAAAAAAAAAAAYIAgT\/xAAnEQABAwEGBgMAAAAAAAAAAAABAAIDEQQFBhJBUQcTIjGRoYGi0v\/aAAwDAQACEQMRAD8AgNd6p5rlV0rznPtRep3duov1L+qCSB8UkGWac9RJ8lWDM24MLsrJJDZvljD+j7SR\/nOLtLnE2c3nh2wE2m5ifBVAHut8mQ6USVeHFzBt3Ag2l0zto2Od9nZW+1nJY8RF3YlJIkEJj9rqqSolc4yOznvvr5RTYFhaYlZUJkJooihov\/\/Z&quot;,&quot;accentcolor&quot;:null,&quot;textcolor&quot;:null,&quot;updateURL&quot;:&quot;https:\/\/www.getpersonas.com\/en-US\/update_check\/475015&quot;,&quot;version&quot;:&quot;1341943165&quot;}'),
+    'mexico' => array('name' => 'Mexico', 'persona' => '{&quot;id&quot;:&quot;475115&quot;,&quot;name&quot;:&quot;Flag of Mexico&quot;,&quot;category&quot;:null,&quot;description&quot;:&quot;&quot;,&quot;author&quot;:&quot;MozThemes&quot;,&quot;username&quot;:&quot;mozthemes&quot;,&quot;headerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475115\/Persona_mexico-1.jpg?1342039329&quot;,&quot;footerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475115\/footer_persona_mexico.jpg?1342039329&quot;,&quot;detailURL&quot;:&quot;http:\/\/www.getpersonas.com\/persona\/475115&quot;,&quot;previewURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475115\/preview.jpg?1342039329&quot;,&quot;iconURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/5\/475115\/preview_small.jpg?1342039329&quot;,&quot;dataurl&quot;:&quot;data:image\/png;base64,\/9j\/4AAQSkZJRgABAQEASABIAAD\/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD\/2wBDAQMDAwQDBAgEBAgQCwkLEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBD\/wAARCAAQABADAREAAhEBAxEB\/8QAFQABAQAAAAAAAAAAAAAAAAAABwb\/xAAgEAABBAMAAgMAAAAAAAAAAAABAgMEEQAFIRIxQXGB\/8QAFwEAAwEAAAAAAAAAAAAAAAAAAwUGCP\/EACERAAICAQIHAAAAAAAAAAAAAAECABEDBBIFEyIxQVHh\/9oADAMBAAIRAxEAPwAs3m20qYz7UmCpbrh8kOtyFUn8JFj7GRaMvmbM1mnzhg6NQ9UPsJ56ll4rAqyfV9w4qooyF93eIcyK45ZWQoKHPL5xepIlzqsaP0gSelaloukpCFd4KNYfm1E54dvM\/9k=&quot;,&quot;accentcolor&quot;:null,&quot;textcolor&quot;:null,&quot;updateURL&quot;:&quot;https:\/\/www.getpersonas.com\/en-US\/update_check\/475115&quot;,&quot;version&quot;:&quot;1342039329&quot;}'),
+    'great_britain' => array('name' => 'Great Britain', 'persona' => '{&quot;id&quot;:&quot;475013&quot;,&quot;name&quot;:&quot;Great Britain Flag&quot;,&quot;category&quot;:null,&quot;description&quot;:&quot;&quot;,&quot;author&quot;:&quot;MozThemes&quot;,&quot;username&quot;:&quot;mozthemes&quot;,&quot;headerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/3\/475013\/Persona_GB1a.jpg?1341942981&quot;,&quot;footerURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/3\/475013\/footer_persona_gb.jpg?1341942981&quot;,&quot;detailURL&quot;:&quot;http:\/\/www.getpersonas.com\/persona\/475013&quot;,&quot;previewURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/3\/475013\/preview.jpg?1341942981&quot;,&quot;iconURL&quot;:&quot;http:\/\/getpersonas-cdn.mozilla.net\/static\/1\/3\/475013\/preview_small.jpg?1341942981&quot;,&quot;dataurl&quot;:&quot;data:image\/png;base64,\/9j\/4AAQSkZJRgABAQEASABIAAD\/2wBDAAMCAgICAgMCAgIDAwMDBAYEBAQEBAgGBgUGCQgKCgkICQkKDA8MCgsOCwkJDRENDg8QEBEQCgwSExIQEw8QEBD\/2wBDAQMDAwQDBAgEBAgQCwkLEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBD\/wAARCAAQABADAREAAhEBAxEB\/8QAFgABAQEAAAAAAAAAAAAAAAAABwAD\/8QAJhAAAAUDAwMFAAAAAAAAAAAAAQIDBAUGBxEAEiEIIkETI1Jykf\/EABYBAQEBAAAAAAAAAAAAAAAAAAUHBP\/EAC8RAAECAwYEAwkAAAAAAAAAAAECAwQFEQAGEhRBYQchcbETMUIWIiU0Q4GCkcH\/2gAMAwEAAhEDEQA\/AEuuZSvTNIyQq+31pqNjoVMWEeWcepHTZBwJkwRUVUAqg8Cb2t4+c6xSly8byCxKkYRqE0qOutq7PILhvCRGcn0W7EOL51IVRW6aJSCOhpp5CxfVNy5FJMrZx1MMmjbAgZjR0WqmmYPjkpGyY\/ohpP2RvLGnFGO06qJ7WHVxG4eSf3ZPLMZGqgkd8dqF6kJdZVWPvTRzCqNxTN1pZqBY+Z2lHaIHUIT0XQBgexZMQHnI6ocfcqBiV5mAUWXNCkn+GvcbWjkqv\/NZcxkYuj8Pq24AtP2B5A7pwne2ziy9lbyLmPZ2r2J5ZXuJCOiFjpDOR4BqqcG7nHkW6qX1HR6o+8cgHxBrMND1CgV+\/I\/kEneyqYK595vknTAvn0qq4yTt9RA6eIBb\/9k=&quot;,&quot;accentcolor&quot;:null,&quot;textcolor&quot;:null,&quot;updateURL&quot;:&quot;https:\/\/www.getpersonas.com\/en-US\/update_check\/475013&quot;,&quot;version&quot;:&quot;1341942981&quot;}')
+);
+
+// Sort by key
+ksort($personas);
+
+// Detect if the user is viewing the page with Firefox.
+// TODO: Detect Firefox version?
+$is_firefox = strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== false;
+?>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link href="styles.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="container">
+      <div id="show-us-msg">Show your support this summer!</div>
+      <div id="customize-msg">Customize Firefox with your nation's flag and join us in celebrating the global spirit of community.</div>
+      <div id="callout-msg">Roll over to try, click to apply. <i class="arrow"></i></div>
+
+      <div id="personas">
+        <ol class="clearfix">
+          <?php foreach ($personas as $id => $data): ?>
+            <li>
+              <img src="images/previews/<?php echo $id; ?>.jpg" persona="<?php echo $data['persona']; ?>">
+              <span><?php echo $data['name']; ?></span>
+            </li>
+          <?php endforeach; ?>
+        </ol>
+      </div>
+
+      <div id="fan-box">
+        <img src="images/browser-stack.png">
+        <div id="see-all-msg">
+          <a href="http://www.getpersonas.com/gallery/Designer/mozthemes">See more Firefox Themes »</a>
+        </div>
+      </div>
+    </div>
+    <script>
+(function() {
+  var aliases = {'PreviewPersona': 'PreviewBrowserTheme',
+           'ResetPersona': 'ResetBrowserThemePreview',
+           'SelectPersona': 'InstallBrowserTheme'};
+  var persona_previews = document.querySelectorAll('#personas img');
+
+  for (var k = 0; k < persona_previews.length; k++) {
+    var item = persona_previews[k];
+    item.setAttribute('data-browsertheme', item.getAttribute('persona'));
+
+    item.addEventListener('mouseover', preview_mouseover, false);
+    item.addEventListener('mouseout', preview_mouseout, false);
+    item.addEventListener('click', preview_click, false);
+  }
+
+  function preview_mouseover(e) {
+    dispatch_persona_event('PreviewPersona', e.target);
+  }
+
+  function preview_mouseout(e) {
+    dispatch_persona_event('ResetPersona', e.target);
+  }
+
+  function preview_click(e) {
+    dispatch_persona_event('SelectPersona', e.target);
+  }
+
+  function dispatch_persona_event(event, preview_node) {
+    dispatch_event(event, preview_node);
+    if (event in aliases) {
+      dispatch_event(aliases[event], preview_node);
+    }
+  }
+
+  function dispatch_event(event, aNode) {
+    var eventObject = document.createEvent("Events");
+    eventObject.initEvent(event, true, false);
+    aNode.dispatchEvent(eventObject);
+  }
+})();
+    </script>
+  </body>
+</html>
